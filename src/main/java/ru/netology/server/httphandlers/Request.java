322eca53e6704b5daaf8b55eefd405d99697fd4d
Roman <61121402+rebrov-rc@users.noreply.github.com>
2023-03-02 15:06:23 +0300

    update pom

--- src/main/java/ru/netology/server/httphandlers/Request.java
+++ src/main/java/ru/netology/server/httphandlers/Request.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.List;
+import org.apache.http.NameValuePair;
 
 public class Request {
 
@@ -11,6 +13,8 @@ public class Request {
     private  BufferedReader in;
     private  String path;
     private  String method;
+    private List<NameValuePair> bodyQuery;
+
 
     public Request(Socket socket) {
         this.socket = socket;
@@ -33,6 +37,36 @@ public class Request {
         }
     }
 
+    private int indexOf(byte[] array, byte[] target, int start, int end) {
+        outer:
+        for (int i = start; i < end; i++) {
+            if (target[0] == array[i]) {
+                for (int j = 0; j < target.length; j++) {
+                    if (target[j] != array[i + j]) {
+                        continue outer;
+                    }
+                }
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public List<NameValuePair> getPostParams() {
+        return this.bodyQuery;
+    }
+
+    public NameValuePair getPostParam(String name) {
+        if (this.bodyQuery != null) {
+            for (NameValuePair el : this.bodyQuery) {
+                if (name.equals(el.getName())) {
+                    return el;
+                }
+            }
+        }
+        return null;
+    }
 
     public String getPath() {
         return this.path;
